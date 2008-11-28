be46f8e9e76b235cdbb0bc0211ccf786fb0dbbf4
trentm <trentm@d98658ec-b809-11dd-8622-756dacf128ff>
2008-11-28 05:05:49 +0000

    more dir struct updates

--- setup.py
+++ setup.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
-# Copyright (c) 2002-2005 ActiveState Corp.
-# Author: Trent Mick (TrentM@ActiveState.com)
+# Copyright (c) 2002-2008 ActiveState Software
+# Author: Trent Mick (trentm@gmail.com)
 
-"""Distutils setup script for 'go'."""
+"""Quick directory changing (super-cd)
 
-import sys
+'go' is a simple command line script to simplify jumping between
+directories in the shell. You can create shortcut names for commonly
+used directories and invoke 'go <shortcut>' to switch to that directory
+-- among other little features.
+"""
+
 import os
+import sys
 from distutils.core import setup
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
+try:
+    import markdown2
+finally:
+    del sys.path[0]
+
+#TODO: Operating System classifiers
+#  Operating System :: OS Independent
+classifiers = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+if sys.version_info < (2, 3):
+    # Distutils before Python 2.3 doesn't accept classifiers.
+    _setup = setup
+    def setup(**kwargs):
+        if kwargs.has_key("classifiers"):
+            del kwargs["classifiers"]
+        _setup(**kwargs)
 
 
 #---- support routines
-
-def _getVersion():
-    import go
-    return go.__version__
 
 def _getBinDir():
     """Return the current Python's bindir."""
@@ -54,25 +80,27 @@ def _getDataFiles():
     if sys.platform.startswith("win"):
         # Dave Quick Search Deskbar integration.
         try:
-            #XXX Convert to string because distutils apparently cannot
-            #    handle unicode paths.
+            # Note: Convert to string because distutils apparently
+            # cannot handle unicode paths.
             dqsdDir = str(_getDQSDInstallDir())
             df.append( (os.path.join(dqsdDir, "localsearches"),
                         ["go.xml"]) )
         except (EnvironmentError, UnicodeError):
             pass
-
     return df
 
 
 #---- setup mainline
 
+doclines = __doc__.split("\n")
+script = (sys.platform == "win32" and "lib\\markdown2.py" or "bin/markdown2")
+
 setup(name="go",
       version=_getVersion(),
-      description="Quick directory changing",
+      description="Quick directory changing (super-cd)",
       author="Trent Mick",
-      author_email="TrentM@ActiveState.com",
-      url="http://trentm.com/projects/go/",
+      author_email="trentm@gmail.com",
+      url="http://code.google.com/p/go-tool/",
       license="MIT License",
       platforms=["Windows", "Linux", "Mac OS X", "Unix"],
       long_description="""\
@@ -87,4 +115,19 @@ used directories and invoke 'go <shortcut>' to switch to that directory
       scripts=_getScripts(),
       data_files=_getDataFiles(),
      )
+setup(
+    name="go",
+    version=go.__version__,
+    maintainer="Trent Mick",
+    maintainer_email="trentm@gmail.com",
+    url="http://code.google.com/p/python-markdown2/",
+    license="http://www.opensource.org/licenses/mit-license.php",
+    platforms=["any"],
+    py_modules=["markdown2"],
+    package_dir={"": "lib"},
+    scripts=scripts,
+    description=doclines[0],
+    classifiers=filter(None, classifiers.split("\n")),
+    long_description="\n".join(doclines[2:]),
+)
 
