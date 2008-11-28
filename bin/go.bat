27576880527fdcb1c01156cb130c7a4ce14f6a5b
trentm <trentm@d98658ec-b809-11dd-8622-756dacf128ff>
2008-11-28 22:56:08 +0000

    tweaked cmd.exe driver

--- bin/go.bat
+++ bin/go.bat
@@ -1,7 +1,6 @@
 @echo off
-rem Copyright (c) 2002-2003 ActiveState Corp.
-rem Author: Trent Mick (TrentM@ActiveState.com)
+rem Windows shell driver for 'go' (http://code.google.com/p/go-tool/).
 set GO_SHELL_SCRIPT=%TEMP%\__tmp_go.bat
-call go.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+call python -m go %1 %2 %3 %4 %5 %6 %7 %8 %9
 if exist %GO_SHELL_SCRIPT% call %GO_SHELL_SCRIPT%
 set GO_SHELL_SCRIPT=
