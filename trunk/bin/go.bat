55c86855573390c69eefa920838a2f610bcb1e11
trentm <trentm@d98658ec-b809-11dd-8622-756dacf128ff>
2008-11-28 22:56:34 +0000

    a start at auto-setup code in go (just Windows for now)

--- trunk/bin/go.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Windows shell driver for 'go' (http://code.google.com/p/go-tool/).
-set GO_SHELL_SCRIPT=%TEMP%\__tmp_go.bat
-call python -m go %1 %2 %3 %4 %5 %6 %7 %8 %9
-if exist %GO_SHELL_SCRIPT% call %GO_SHELL_SCRIPT%
-set GO_SHELL_SCRIPT=
--- trunk/lib/go.py
+++ trunk/lib/go.py
@@ -67,6 +67,18 @@ import xml.dom.minidom
 class GoError(Exception):
     pass
 
+class InternalGoError(GoError):
+    def __str__(self):
+        return Error.__str__(self) + """
+
+* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* Please log a bug at                                 *
+*    http://code.google.com/p/go-tool/issues/list     *
+* to report this error. Thanks!                       *
+* -- Trent                                            *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * *"""
+
+
 
 #---- globals
 
@@ -78,6 +90,28 @@ if sys.platform.startswith("win") and\
    os.path.splitext(sys.executable)[0][-1] == 'w':
     _subsystem = "windows"
     
+    
+_gDriverFromShell = {
+    "cmd": """
+@echo off
+rem Windows shell driver for 'go' (http://code.google.com/p/go-tool/).
+set GO_SHELL_SCRIPT=%TEMP%\__tmp_go.bat
+call python -m go %1 %2 %3 %4 %5 %6 %7 %8 %9
+if exist %GO_SHELL_SCRIPT% call %GO_SHELL_SCRIPT%
+set GO_SHELL_SCRIPT=
+""",
+    "bash": """
+# Bash shell driver for 'go' (http://code.google.com/p/go-tool/).
+function go {
+    export GO_SHELL_SCRIPT=$HOME/.__tmp_go.sh
+    python -m go $*
+    if [ -f $GO_SHELL_SCRIPT ] ; then
+        source $GO_SHELL_SCRIPT
+    fi
+}
+""",
+}
+
 
 
 #---- public module interface
@@ -319,6 +353,183 @@ def error(msg):
                          "platform, '%s'." % (_subsystem, sys.platform))
 
 
+def _getShell():
+    if sys.platform == "win32":
+        #assert "cmd.exe" in os.environ["ComSpec"]
+        return "cmd"
+    elif "BASH" in os.environ:
+        return "bash"
+    elif "tcsh" in os.environ:
+        return "csh"
+    else:
+        raise InternalGoError("couldn't determine your shell")
+
+def setup():
+    from os.path import normcase, normpath, join
+
+    shell = _getShell()
+    try:
+        driver = _gDriverFromShell[shell]
+    except KeyError:
+        raise InternalGoError("don't know how to setup for your shell: %s"
+                              % shell)
+
+    # Knowing the user's HOME dir will help later.
+    nhome = None
+    if "HOME" in os.environ:
+        nhome = _normpath(os.environ["HOME"])
+    elif "HOMEDRIVE" in os.environ and "HOMEPATH" in os.environ:
+        nhome = _normpath(
+            os.environ["HOMEDRIVE"] + os.environ["HOMEPATH"])
+
+    print "* * *"
+
+
+    if shell == "cmd":
+        # Need a install candidate dir for "go.bat".
+        nprefix = _normpath(sys.prefix)
+        ncandidates = set()
+        candidates = []
+        for dir in os.environ["PATH"].split(os.path.pathsep):
+            ndir = _normpath(dir)
+            if ndir.startswith(nprefix):
+                if ndir not in ncandidates:
+                    ncandidates.add(ndir)
+                    candidates.append(dir)
+            elif nhome and ndir.startswith(nhome) \
+                 and ndir[len(nhome)+1:].count(os.path.sep) < 2:
+                if ndir not in ncandidates:
+                    ncandidates.add(ndir)
+                    candidates.append(dir)
+        #print candidates
+
+        print """\
+It appears that `go' is not setup properly in your environment. Typing
+`go' must end up calling `go.bat' somewhere on your PATH and *not* `go.py'
+directly. This is how `go' can change the directory in your current shell.
+
+You'll need a file "go.bat" with the following contents in a directory on
+your PATH:
+
+%s""" % _indent(driver)
+
+        if candidates:
+            print "Candidate directories are:\n"
+            for i, dir in enumerate(candidates):
+                print "  [%s] %s" % (i+1, dir)
+
+            print
+            answer = _query_custom_answers(
+                "If you would like this script to create `go.bat' for you in\n"
+                    "one of these directories, enter the number of that\n"
+                    "directory. Otherwise, enter 'no' to not create `go.bat'.",
+                [str(i+1) for i in range(len(candidates))] + ["&no"],
+                default="no",
+            )
+            if answer == "no":
+                pass
+            else:
+                dir = candidates[int(answer)-1]
+                path = join(dir, "go.bat")
+                print "\nCreating `%s'." % path
+                print "You should now be able to run `go --help'."
+                open(path, 'w').write(driver)
+    else:
+        #TODO: other shells
+        XXX
+    
+    print "* * *"
+
+
+# Recipe: query_custom_answers (1.0)
+def _query_custom_answers(question, answers, default=None):
+    """Ask a question via raw_input() and return the chosen answer.
+    
+    @param question {str} Printed on stdout before querying the user.
+    @param answers {list} A list of acceptable string answers. Particular
+        answers can include '&' before one of its letters to allow a
+        single letter to indicate that answer. E.g., ["&yes", "&no",
+        "&quit"]. All answer strings should be lowercase.
+    @param default {str, optional} A default answer. If no default is
+        given, then the user must provide an answer. With a default,
+        just hitting <Enter> is sufficient to choose. 
+    """
+    prompt_bits = []
+    answer_from_valid_choice = {
+        # <valid-choice>: <answer-without-&>
+    }
+    clean_answers = []
+    for answer in answers:
+        if '&' in answer and not answer.index('&') == len(answer)-1:
+            head, sep, tail = answer.partition('&')
+            prompt_bits.append(head.lower()+tail.lower().capitalize())
+            clean_answer = head+tail
+            shortcut = tail[0].lower()
+        else:
+            prompt_bits.append(answer.lower())
+            clean_answer = answer
+            shortcut = None
+        if default is not None and clean_answer.lower() == default.lower():
+            prompt_bits[-1] += " (default)"
+        answer_from_valid_choice[clean_answer.lower()] = clean_answer
+        if shortcut:
+            answer_from_valid_choice[shortcut] = clean_answer
+        clean_answers.append(clean_answer.lower())
+
+    # This is what it will look like:
+    #   Frob nots the zids? [Yes (default), No, quit] _
+    # Possible alternatives:
+    #   Frob nots the zids -- Yes, No, quit? [y] _
+    #   Frob nots the zids? [*Yes*, No, quit] _
+    #   Frob nots the zids? [_Yes_, No, quit] _
+    #   Frob nots the zids -- (y)es, (n)o, quit? [y] _
+    prompt = " [%s] " % ", ".join(prompt_bits)
+    leader = question + prompt
+    if len(leader) + max(len(c) for c in answer_from_valid_choice) > 78:
+        leader = question + '\n' + prompt.lstrip()
+    leader = leader.lstrip()
+
+    valid_choices = answer_from_valid_choice.keys()
+    admonishment = "*** Please respond with '%s' or '%s'. ***" \
+                   % ("', '".join(clean_answers[:-1]), clean_answers[-1])
+
+    while 1:
+        sys.stdout.write(leader)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in answer_from_valid_choice:
+            return answer_from_valid_choice[choice]
+        else:
+            sys.stdout.write("\n"+admonishment+"\n\n\n")
+
+
+
+# Recipe: indent (0.2.1)
+def _indent(s, width=4, skip_first_line=False):
+    """_indent(s, [width=4]) -> 's' indented by 'width' spaces
+
+    The optional "skip_first_line" argument is a boolean (default False)
+    indicating if the first line should NOT be indented.
+    """
+    lines = s.splitlines(1)
+    indentstr = ' '*width
+    if skip_first_line:
+        return indentstr.join(lines)
+    else:
+        return indentstr + indentstr.join(lines)
+
+
+def _normpath(path):
+    from os.path import normcase, normpath
+    n = normcase(normpath(path))
+    if n.endswith(os.path.sep):
+        n = n[:-1]
+    elif os.path.altsep and n.endswith(os.path.altsep):
+        n = n[:-1]
+    return n
+
+
 #---- mainline
 
 def main(argv):
@@ -329,34 +540,36 @@ def main(argv):
     except KeyError:
         if _subsystem == "windows":
             pass # Don't complain about missing console setup.
-        elif sys.platform.startswith('win'):
-            sys.stderr.write("""* * *
-It appears that 'go' is not setup properly in your environment. Typing
-'go' should end up calling go.bat somewhere on your PATH and *not* go.py
-directly. Both go.bat and go.bat should be installed automatically by
-the setup.py script.
-* * *
-""")
-            return 1
-        else:
-            sys.stderr.write(r"""* * *
-It appears that 'go' is not setup properly in your environment.  If you
-are using the Bash shell you should have the following function in your
-environment:
-
-    function go {
-        export GO_SHELL_SCRIPT=$HOME/.__tmp_go.sh
-        python -m go $*
-        if [ -f $GO_SHELL_SCRIPT ] ; then
-            source $GO_SHELL_SCRIPT
-        fi
-    }
-
-You should add the above function to your ~/.bashrc file or just cut and
-paste the function into your current shell.
-* * *
-""")
-            return 1
+        return setup()
+#        XXX
+#        if sys.platform.startswith('win'):
+#            sys.stderr.write("""* * *
+#It appears that 'go' is not setup properly in your environment. Typing
+#'go' should end up calling go.bat somewhere on your PATH and *not* go.py
+#directly. Both go.bat and go.py should be installed automatically by
+#the setup.py script.
+#* * *
+#""")
+#            return 1
+#        else:
+#            sys.stderr.write(r"""* * *
+#It appears that 'go' is not setup properly in your environment.  If you
+#are using the Bash shell you should have the following function in your
+#environment:
+#
+#    function go {
+#        export GO_SHELL_SCRIPT=$HOME/.__tmp_go.sh
+#        python -m go $*
+#        if [ -f $GO_SHELL_SCRIPT ] ; then
+#            source $GO_SHELL_SCRIPT
+#        fi
+#    }
+#
+#You should add the above function to your ~/.bashrc file or just cut and
+#paste the function into your current shell.
+#* * *
+#""")
+#            return 1
     else:
         generateShellScript(shellScript) # no-op, overwrite old one
 
